6591a12371c0477779fe78f2078ada8a8d98d927
gianpaolobasilico <Gia120992>
2020-04-28 00:10:32 +0200

    increased js memory allocation

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [{
     name: "nod",
     script: "./backend/dist/app.js",
+    node_args : "--max_old_space_size=480",
     instances: "max",
     exec_mode : "cluster",
     env: {

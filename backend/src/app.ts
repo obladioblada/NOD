1d1bacfe3ef9d34c108069de3ceccc5fb189d685
gianpaolobasilico <Gia120992>
2020-05-02 23:22:47 +0200

    removed returning index

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -35,9 +35,6 @@ if (process.env.NODE_ENV === "production") {
     logger.info(" production binding angular");
     let frontDistDir = path.join(__dirname, '/../../dist/');
     app.use(express.static(frontDistDir));
-    app.get("*", (_req, res) => {
-        res.sendFile(frontDistDir + "index.html");
-    });
 }
 
 process.title = "nod-backend";

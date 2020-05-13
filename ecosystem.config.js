a7218465c4f6e086e78b4b3d8308cbfa963c7b1a
gianpaolobasilico <Gia120992>
2020-05-13 20:21:40 +0200

    changed path ecosystem.config.jj

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: "nod",
-    script: "./backend/dist/app.js",
+    script: "./backend/dist/backend/src/app.js",
     node_args : "--max_old_space_size=480",
     instances: "max",
     exec_mode : "cluster",
